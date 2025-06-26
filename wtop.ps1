c7c9de40964d508935b4187e0f7cb443c58dbb94
Rich Smith <richrsmith@proton.me>
2025-06-26 13:08:13 -0400

    License material added

--- wtop.ps1
+++ wtop.ps1
@@ -1,4 +1,20 @@
-# This is a prototype version for a PowerShell version of the Linux program Top, currently this has a varied number of bugs and almost no functionality.
+# WTop - a Top like script for PowerShell
+# Copyright (C) 2025 Richard Smith (Islc12)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+# This is a prototype version for the PowerShell version of the Linux program Top, currently this has a varied number of bugs and almost no functionality.
 # However, it does still work, even if on the most basic of levels and will provide the user with a baseline of information such as PID, Process Name, 
 # CPU%, Memory usage, NPM, and start time. As time goes on I will continue to work on this script, hopefully building on it in such a manner that it can
 # be adequately used across servers through use of cmdlets such as Invoke-Command.
